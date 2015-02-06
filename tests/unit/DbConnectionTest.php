3d9e37970293a72f6cfa8db63361b72daff706aa
Chapabu <chapabu101@gmail.com>
2015-02-06 16:39:06 +0000

    Initial commit of Database tests

---
+++ tests/unit/DbConnectionTest.php
@@ -0,0 +1,48 @@
+<?php
+
+use Codeception\Module\Drupal;
+use Codeception\Util\Fixtures;
+
+/**
+ * Class DbConnectionTestCest
+ */
+class DbConnectionTest extends \Codeception\TestCase\Test
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    /**
+     * @var \Codeception\Module\Drupal
+     */
+    protected $module;
+
+    /**
+     * @var array
+     */
+    protected $validConfig = [];
+
+
+    /**
+     * { @inheritdoc }
+     */
+    protected function _before()
+    {
+        $this->validConfig = Fixtures::get('validModuleConfig');
+
+        $this->module = new Drupal;
+        $this->module->_setConfig($this->validConfig);
+        $this->module->_initialize();
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_access_the_database()
+    {
+        $entityInfo = entity_get_info();
+
+        $this->assertCount(6, $entityInfo, 'Entity info returned.');
+    }
+}
