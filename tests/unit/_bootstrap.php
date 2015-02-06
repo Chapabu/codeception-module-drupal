3c51c7a0eee7857867e100eb77377e8ee746d14d
Chapabu <chapabu101@gmail.com>
2015-02-06 16:37:14 +0000

    Removed Fixtures

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -3,15 +3,3 @@
 /**
  * Here you can initialize variables that will be used for your tests
  */
-use Codeception\Util\Fixtures;
-
-$mockValidModuleConfig = [
-    'root' => 'vendor/drupal/drupal'
-];
-
-$mockInvalidModuleconfig = [
-    'root' => 'this/is/a/fake/path'
-];
-
-Fixtures::add('validModuleconfig', $mockValidModuleConfig);
-Fixtures::add('invalidModuleConfig', $mockInvalidModuleconfig);
