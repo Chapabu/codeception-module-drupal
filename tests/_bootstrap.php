62730a40076394b1870d24705edb71d1316ba53b
Chapabu <chapabu101@gmail.com>
2015-02-11 18:39:49 +0000

    Fixed indentation. Added fixture for config with a valid path but invalid module list.

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -4,9 +4,24 @@
 use Codeception\Util\Fixtures;
 
 Fixtures::add('validModuleConfig', [
-  'root' => 'testsites/drupal7/drupal-7.x'
+    'root' => 'testsites/drupal7/drupal-7.x',
+    'submodules' => [
+        'EntityAssertions'
+    ]
 ]);
 
 Fixtures::add('invalidModuleConfig', [
-  'root' => 'this/is/a/fake/path'
+    'root' => 'this/is/a/fake/path',
+    'submodules' => [
+        'dummy'
+    ]
+]);
+
+
+Fixtures::add('validPathInvalidModules', [
+    'root' => 'testsites/drupal7/drupal-7.x',
+    'submodules' => [
+        'entity'
+    ]
+
 ]);
