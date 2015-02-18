50e665dd569d9ee5fefe606ce48022d6e7c8d783
Chapabu <chapabu101@gmail.com>
2015-02-18 22:57:49 +0000

    Added count assertion.

--- tests/_support/UnitHelper.php
+++ tests/_support/UnitHelper.php
@@ -7,4 +7,16 @@ namespace Codeception\Module;
 class UnitHelper extends \Codeception\Module
 {
 
+    public function assertCount($countable, $expected, $message = '')
+    {
+        if (!is_int($expected)) {
+            $this->fail('Can\'t count against ' . $expected);
+        }
+
+        if (!$countable instanceof \Countable && !$countable instanceof \Traversable && !is_array($countable)) {
+            $this->fail($countable. ' is not countable.');
+        }
+
+        return $this->assertEquals(count($countable), $expected, $message);
+    }
 }
