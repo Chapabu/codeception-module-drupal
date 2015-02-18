c7c0d7df93d4a1d095d10838ccff977aa12e9243
Chapabu <chapabu101@gmail.com>
2015-02-18 23:58:46 +0000

    Removed _before

--- tests/unit/Drupal7/Drupal7AssertionCestBase.php
+++ tests/unit/Drupal7/Drupal7AssertionCestBase.php
@@ -1,11 +1,10 @@
 <?php namespace Drupal7;
 
+use Codeception\Module\Drupal7\Drupal7 as Drupal;
+use Codeception\Util\Fixtures;
 use UnitTester;
 
 abstract class Drupal7AssertionCestBase
 {
 
-    public function _before(UnitTester $I)
-    {
-    }
 }
