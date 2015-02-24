c3a58d0f16ed6ae910c15165e44f866998cbf448
Chapabu <chapabu101@gmail.com>
2015-02-24 08:39:06 +0000

    Added bundle assertions. Added docblocks for seeEntityExists and dontSeeEntityExists.

--- src/Module/Drupal7/Submodules/EntityTrait.php
+++ src/Module/Drupal7/Submodules/EntityTrait.php
@@ -25,6 +25,12 @@ trait EntityTrait
         return entity_get_info($entityType);
     }
 
+    /**
+     * Check that an entity type exists.
+     *
+     * @param string $entityMachineName
+     *   The machine name of the entity you are testing for (i.e. node)
+     */
     public function seeEntityExists($entityMachineName)
     {
         $entity = $this->grabEntityInfo($entityMachineName);
@@ -32,10 +38,50 @@ trait EntityTrait
         $this->assertNotNull($entity);
     }
 
+    /**
+     * Check that an entity type does not exist.
+     *
+     * @param string $entityMachineName
+     *   The machine name of the entity you are testing for (i.e. node)
+     */
     public function dontSeeEntityExists($entityMachineName)
     {
         $entity = $this->grabEntityInfo($entityMachineName);
 
         $this->assertNull($entity);
+    }
+
+    /**
+     * Check that an entity type has a specific bundle.
+     *
+     * @param $entityMachineName
+     *    The machine name of the entity you are testing (i.e. node)
+     * @param $bundleMachineName
+     *    The machine name of the bundle you are testing for (i.e. article)
+     */
+    public function seeEntityHasBundle($entityMachineName, $bundleMachineName)
+    {
+        $entityInfo = $this->grabEntityInfo($entityMachineName);
+
+        if (!array_key_exists($bundleMachineName, $entityInfo['bundles'])) {
+            $this->fail('Bundle ' . $bundleMachineName . ' does not exist on the ' . $entityMachineName . ' entity.');
+        }
+    }
+
+    /**
+     * Check that an entity type does not have a specific bundle.
+     *
+     * @param $entityMachineName
+     *    The machine name of the entity you are testing (i.e. node)
+     * @param $bundleMachineName
+     *    The machine name of the bundle you are testing for (i.e. article)
+     */
+    public function dontSeeEntityHasBundle($entityMachineName, $bundleMachineName)
+    {
+        $entityInfo = $this->grabEntityInfo($entityMachineName);
+
+        if (array_key_exists($bundleMachineName, $entityInfo['bundles'])) {
+            $this->fail('Bundle ' . $bundleMachineName . ' exists on the ' . $entityMachineName . ' entity.');
+        }
     }
 }
