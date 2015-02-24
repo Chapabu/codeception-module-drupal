844d6c161603a456582e9e5d2d1a093616d80509
Chapabu <chapabu101@gmail.com>
2015-02-24 18:17:36 +0000

    Added bundle assertion tests

--- tests/unit/Drupal7/EntityAssertionsCest.php
+++ tests/unit/Drupal7/EntityAssertionsCest.php
@@ -1,6 +1,8 @@
 <?php namespace Drupal7;
 
+use PHPUnit_Framework_AssertionFailedError;
 use \UnitTester;
+use Mockery as Mock;
 
 class EntityAssertionsCest extends Drupal7AssertionCestBase
 {
@@ -22,8 +24,43 @@ class EntityAssertionsCest extends Drupal7AssertionCestBase
         $I->seeEntityExists('node');
     }
 
-    public function it_can_see_if_an_entity_does_not_exist(UnitTester $I)
+    public function it_can_check_if_an_entity_does_not_exist(UnitTester $I)
     {
-        $I->dontSeeEntityExists('fake_entity');
+        $I->dontSeeEntityExists('nodes');
+    }
+
+    public function it_can_check_if_an_entity_has_a_bunde(UnitTester $I)
+    {
+        $I->seeEntityHasBundle('node', 'article');
+        $I->seeEntityHasBundle('node', 'page');
+
+    }
+
+    public function see_fails_if_looking_for_a_non_existent_bundle(UnitTester $I)
+    {
+        try {
+            $I->seeEntityHasBundle('node', 'fakebundle');
+        } catch (\PHPUnit_Framework_AssertionFailedError $e) {
+            return;
+        }
+
+        $I->fail('seeEntityHasBundle does not fail with a non-existent bundle');
+    }
+
+    public function it_can_check_if_an_entity_does_not_have_a_bunde(UnitTester $I)
+    {
+        $I->dontSeeEntityHasBundle('node', 'fakebundle');
+        $I->dontSeeEntityHasBundle('node', 'fakebundle2');
+    }
+
+    public function dont_see_fails_if_looking_for_an_existing_bundle(UnitTester $I)
+    {
+        try {
+            $I->dontSeeEntityHasBundle('node', 'article');
+        } catch (\PHPUnit_Framework_AssertionFailedError $e) {
+            return;
+        }
+
+        $I->fail('dontSeeEntityHasBundle does not fail with an existing bundle');
     }
 }
