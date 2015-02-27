7c1740d36c4853b9105485fb24309bdf0c5de656
Chapabu <chapabu101@gmail.com>
2015-02-27 23:57:18 +0000

    Added check for entity base fields

--- src/Module/Drupal7/Submodules/EntityTrait.php
+++ src/Module/Drupal7/Submodules/EntityTrait.php
@@ -58,6 +58,9 @@ trait EntityTrait
      *    The machine name of the entity you are testing (i.e. node)
      * @param $bundleMachineName
      *    The machine name of the bundle you are testing for (i.e. article)
+     *
+     * @return bool|void
+     *     Will return true if test passes or void if it fails.
      */
     public function seeEntityHasBundle($entityMachineName, $bundleMachineName)
     {
@@ -79,6 +82,9 @@ trait EntityTrait
      *    The machine name of the entity you are testing (i.e. node)
      * @param $bundleMachineName
      *    The machine name of the bundle you are testing for (i.e. article)
+     *
+     * @return bool|void
+     *     Will return true if test passes or void if it fails.
      */
     public function dontSeeEntityHasBundle($entityMachineName, $bundleMachineName)
     {
@@ -91,5 +97,39 @@ trait EntityTrait
         $failMessage = 'Bundle ' . $bundleMachineName . ' exists on the ' . $entityMachineName . ' entity.';
 
         return $this->fail($failMessage);
+    }
+
+    /**
+     * Check that an entity type has a base field.
+     *
+     * @param string $entityMachineName
+     *    The machine name of the entity you are testing (i.e. node)
+     * @param string $baseField
+     *    The name of the base field you are looking for.
+     * @param string $schemaKey
+     *    The key of the array item that contains the entity base fields.
+     */
+    public function seeEntityHasBaseField($entityMachineName, $baseField, $schemaKey = 'schema_fields_sql')
+    {
+        $entityInfo = $this->grabEntityInfo($entityMachineName);
+
+        $this->assertContains($baseField, $entityInfo[$schemaKey][$baseField]);
+    }
+
+    /**
+     * Check that an entity type does not have a base field.
+     *
+     * @param string $entityMachineName
+     *    The machine name of the entity you are testing (i.e. node)
+     * @param string $baseField
+     *    The name of the base field you are looking for.
+     * @param string $schemaKey
+     *    The key of the array item that contains the entity base fields.
+     */
+    public function dontSeeEntityHasBaseField($entityMachineName, $baseField, $schemaKey = 'schema_fields_sql')
+    {
+        $entityInfo = $this->grabEntityInfo($entityMachineName);
+
+        $this->assertNotContains($baseField, $entityInfo[$schemaKey][$baseField]);
     }
 }
