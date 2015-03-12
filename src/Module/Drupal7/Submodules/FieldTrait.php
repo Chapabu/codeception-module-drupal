bc2968097877e042457da58c988111db3240fa65
Chapabu <chapabu101@gmail.com>
2015-03-12 18:46:58 +0000

    Removed function check for field_info_field_map().

--- src/Module/Drupal7/Submodules/FieldTrait.php
+++ src/Module/Drupal7/Submodules/FieldTrait.php
@@ -8,44 +8,6 @@ trait FieldTrait
 {
 
     /**
-     * Grab a list of all fields.
-     *
-     * This is simply a wrapper around field_info_field_map().
-     *
-     * @return array
-     *   Information about an entity type, or all entities if one was passed in.
-     */
-    public function grabFieldList()
-    {
-        // Drupal 7 prior to 7.22 doesn't have the field_info_field_map function, so we need to manually implement if
-        // if it doesn't exist. We could just use fallback, but I'd rather stick with core functions where I can.
-        if (function_exists('field_info_field_map')) {
-            return field_info_field_map();
-        }
-
-        // ToDo: Implement manual fallback for Drupal < 7.22.
-    }
-
-    /**
-     * Grab the information about a field instance attached to a bundle.
-     *
-     * This is simple a wrapper around field_info_instance.
-     *
-     * @param string $entityType
-     *   The entity type the bundle is attached to.
-     * @param string $fieldName
-     *   The field name (i.e. field_image)
-     * @param string $bundleName
-     *   The bundle name you are looking on.
-     * @return array|null
-     *   An array containing the output of field_info_instance or null if the field instance doesn't exist.
-     */
-    public function grabFieldInstance($entityType, $fieldName, $bundleName)
-    {
-        return field_info_instance($entityType, $fieldName, $bundleName);
-    }
-
-    /**
      * Check that a field base exists.
      *
      * @param string $fieldName
@@ -56,6 +18,19 @@ trait FieldTrait
         $fieldList = $this->grabFieldList();
 
         $this->assertContains($fieldName, array_keys($fieldList));
+    }
+
+    /**
+     * Grab a list of all fields.
+     *
+     * This is simply a wrapper around field_info_field_map().
+     *
+     * @return array
+     *   Information about an entity type, or all entities if one was passed in.
+     */
+    public function grabFieldList()
+    {
+        return field_info_field_map();
     }
 
     /**
@@ -88,6 +63,25 @@ trait FieldTrait
         $fieldInfo = $this->grabFieldInstance($entityType, $fieldName, $bundleName);
 
         $this->assertNotNull($fieldInfo);
+    }
+
+    /**
+     * Grab the information about a field instance attached to a bundle.
+     *
+     * This is simple a wrapper around field_info_instance.
+     *
+     * @param string $entityType
+     *   The entity type the bundle is attached to.
+     * @param string $fieldName
+     *   The field name (i.e. field_image)
+     * @param string $bundleName
+     *   The bundle name you are looking on.
+     * @return array|null
+     *   An array containing the output of field_info_instance or null if the field instance doesn't exist.
+     */
+    public function grabFieldInstance($entityType, $fieldName, $bundleName)
+    {
+        return field_info_instance($entityType, $fieldName, $bundleName);
     }
 
     /**
